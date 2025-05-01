5c6e62d546a95c3b470430dcb7ff17c862b02d86
velkani-s <velkani2021@gmail.com>
2025-05-01 13:33:03 +0530

    build sari pandrom

--- src/Components/DoctorRegister/DoctorLogin/DoctorLogin.jsx
+++ src/Components/DoctorRegister/DoctorLogin/DoctorLogin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Doctorlogin.css";
+
 import supabase from "../../../config/supabaseClient";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
